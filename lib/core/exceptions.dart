68df4eca8c0cc0b1c14141fa8c2db3962bb8987d
Gianluca Romeo <gianlucaromeo@outlook.com>
2024-04-11 22:44:04 +0200

    Added missing files

--- lib/core/exceptions.dart
+++ lib/core/exceptions.dart
@@ -4,6 +4,11 @@ abstract class ServerException implements Exception {
   Failure get failure;
 }
 
+class GenericException extends ServerException {
+  @override
+  Failure get failure => GenericFailure();
+}
+
 abstract class CacheException implements Exception {
   Failure get failure;
 }
