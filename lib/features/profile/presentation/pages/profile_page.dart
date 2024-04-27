345b258ee2e62099927dc4160e93206c41e4857a
gianlucaromeo <gianlucaromeo@outlook.com>
2024-04-27 23:37:56 +0200

    Edited profile page

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lectura/core/extensions.dart';
 import 'package:lectura/features/auth/bloc/login/login_bloc.dart';
+import 'package:lectura/features/common/bloc/theme/theme_bloc.dart';
 import 'package:lectura/features/common/presentation/pages/page_skeleton.dart';
 
 @RoutePage()
@@ -10,23 +12,42 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final loginBloc = context.read<LoginBloc>();
     return LecturaPage(
-      title: "***PROFILE",
-      body: Column(
-        children: [
-          MaterialButton(
-            child: const Text("***Logout"),
-            onPressed: () {
-              context.read<LoginBloc>().add(UserLoggedOut());
-            },
-          ),
-          MaterialButton(
-            child: const Text("***Delete account"),
-            onPressed: () {
-              context.read<LoginBloc>().add(UserDeleteAccountRequested());
-            },
-          ),
-        ],
+      title: context.l10n.profile_page__title,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(loginBloc.state.user!.email!),
+            25.0.verticalSpace,
+            OutlinedButton(
+              child: Text(context.l10n.profile_page__logout),
+              onPressed: () {
+                loginBloc.add(UserLoggedOut());
+              },
+            ),
+            15.0.verticalSpace,
+            OutlinedButton(
+              child: Text(
+                context.l10n.profile_page__delete_account,
+                style: TextStyle(color: Theme.of(context).colorScheme.error),
+              ),
+              onPressed: () {
+                loginBloc.add(UserDeleteAccountRequested());
+              },
+            ),
+            25.0.verticalSpace,
+            IconButton(
+              onPressed: () {
+                context.read<ThemeBloc>().add(ThemeToggled());
+              },
+              icon: context.watch<ThemeBloc>().state.currentThemeMode == ThemeMode.dark
+                  ? const Icon(Icons.light_mode_outlined)
+                  : const Icon(Icons.dark_mode_outlined),
+            ),
+          ],
+        ),
       ),
     );
   }
