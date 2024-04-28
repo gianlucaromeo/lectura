0dd54ec10cb08d2f5287187c415248ac9dc7a5e7
gianlucaromeo <gianlucaromeo@outlook.com>
2024-04-28 23:39:37 +0200

    fixed warnings and updated todo

--- lib/features/common/presentation/widgets/login_bloc_consumer_with_loading.dart
+++ lib/features/common/presentation/widgets/login_bloc_consumer_with_loading.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -28,8 +26,6 @@ class _LoginBlocConsumerWithLoadingState
   Widget build(BuildContext context) {
     return BlocConsumer<LoginBloc, LoginState>(
       listener: (context, state) {
-        log("State: ${state.status}");
-
         // Add/Remove loading
         if (state.status == LoginStatus.inProgress && !isLoading) {
           setState(() {
