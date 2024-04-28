6e935de66255bb4ee3d1292f60bb4dcee1d4c88d
gianlucaromeo <gianlucaromeo@outlook.com>
2024-04-28 20:17:47 +0200

    Fixed BrowseBloc building when hot restarting

--- lib/features/common/presentation/pages/logged_user_page.dart
+++ lib/features/common/presentation/pages/logged_user_page.dart
@@ -8,10 +8,6 @@ import 'package:lectura/core/routes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lectura/features/auth/bloc/login/login_bloc.dart';
 import 'package:lectura/features/search/presentation/bloc/browse_bloc.dart';
-import 'package:lectura/features/common/data/datasources/user_books_datasource.dart';
-import 'package:lectura/features/common/data/repositories/user_books_repository.dart';
-import 'package:lectura/features/search/data/datasources/search_datasource.dart';
-import 'package:lectura/features/search/data/repositories/search_repository.dart';
 
 @RoutePage()
 class LoggedUserPage extends StatelessWidget {
@@ -19,68 +15,49 @@ class LoggedUserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final googleApiDataSource = GoogleApiDataSource();
-
-    final searchRepository = SearchRepositoryImpl(
-      googleApiDataSource,
-    );
-
-    final userBooksRepository = FirebaseUserBooksRepository(
-      FirebaseUserBooksDatasource(googleApiDataSource),
-      googleApiDataSource,
-    );
-
-    final browseBloc = BrowseBloc(
-      searchRepository,
-      userBooksRepository,
-    );
-
-    browseBloc.add(FetchUserBooksRequested(
+    context.read<BrowseBloc>().add(FetchUserBooksRequested(
       context.read<LoginBloc>().state.user!.id!,
     ));
 
-    return BlocProvider.value(
-      value: browseBloc,
-      child: AutoTabsScaffold(
-        routes: [
-          Routes.libraryWrapperRoute,
-          Routes.searchWrapperRoute,
-          Routes.profileRoute,
-        ],
-        bottomNavigationBuilder: (_, tabsRouter) {
-          log(
-            "Current index: ${tabsRouter.activeIndex}, route: ${tabsRouter.currentPath}",
-            name: "LoggedUserPage",
-          );
+    return AutoTabsScaffold(
+      routes: [
+        Routes.libraryWrapperRoute,
+        Routes.searchWrapperRoute,
+        Routes.profileRoute,
+      ],
+      bottomNavigationBuilder: (_, tabsRouter) {
+        log(
+          "Current index: ${tabsRouter.activeIndex}, route: ${tabsRouter.currentPath}",
+          name: "LoggedUserPage",
+        );
 
-          log(
-            "Stack ${tabsRouter.stack.map((e) => e.name)}",
-            name: "LoggedUserPage",
-          );
+        log(
+          "Stack ${tabsRouter.stack.map((e) => e.name)}",
+          name: "LoggedUserPage",
+        );
 
-          return tabsRouter.currentPath.endsWith("/book")
-              ? 0.0.verticalSpace
-              : BottomNavigationBar(
-                  showUnselectedLabels: false,
-                  items: [
-                    BottomNavigationBarItem(
-                      icon: const Icon(Icons.book),
-                      label: context.l10n.bottom_bar__library,
-                    ),
-                    BottomNavigationBarItem(
-                      icon: const Icon(Icons.search),
-                      label: context.l10n.bottom_bar__search,
-                    ),
-                    BottomNavigationBarItem(
-                      icon: const Icon(Icons.person),
-                      label: context.l10n.bottom_bar__profile,
-                    ),
-                  ],
-                  currentIndex: tabsRouter.activeIndex,
-                  onTap: tabsRouter.setActiveIndex,
-                );
-        },
-      ),
+        return tabsRouter.currentPath.endsWith("/book")
+            ? 0.0.verticalSpace
+            : BottomNavigationBar(
+                showUnselectedLabels: false,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: const Icon(Icons.book),
+                    label: context.l10n.bottom_bar__library,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: const Icon(Icons.search),
+                    label: context.l10n.bottom_bar__search,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: const Icon(Icons.person),
+                    label: context.l10n.bottom_bar__profile,
+                  ),
+                ],
+                currentIndex: tabsRouter.activeIndex,
+                onTap: tabsRouter.setActiveIndex,
+              );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,15 @@ import 'package:lectura/features/auth/data/datasources/auth_remote_datasource.da
 import 'package:lectura/features/auth/data/repositories/auth_repository.dart';
 import 'package:lectura/features/auth/domain/repositories/auth_repository.dart';
 import 'package:lectura/features/common/bloc/theme/theme_bloc.dart';
+import 'package:lectura/features/search/presentation/bloc/browse_bloc.dart';
 import 'package:lectura/firebase_options.dart';
 import 'package:lectura/core/app_env.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:lectura/features/common/data/datasources/user_books_datasource.dart';
+import 'package:lectura/features/common/data/repositories/user_books_repository.dart';
+import 'package:lectura/features/search/data/datasources/search_datasource.dart';
+import 'package:lectura/features/search/data/repositories/search_repository.dart';
 
 // Call this from one of the class of the "/main" folder
 Future<void> mainCommon(AppEnvironment environment) async {
@@ -35,6 +41,17 @@ Future<void> mainCommon(AppEnvironment environment) async {
     ),
   );
 
+  final googleApiDataSource = GoogleApiDataSource();
+
+  final searchRepository = SearchRepositoryImpl(
+    googleApiDataSource,
+  );
+
+  final userBooksRepository = FirebaseUserBooksRepository(
+    FirebaseUserBooksDatasource(googleApiDataSource),
+    googleApiDataSource,
+  );
+
   runApp(
     RepositoryProvider<AuthRepository>.value(
       value: authRepository,
@@ -45,6 +62,12 @@ Future<void> mainCommon(AppEnvironment environment) async {
           ),
           BlocProvider(
             create: (context) => LoginBloc(authRepository: authRepository),
+          ),
+          BlocProvider(
+            create: (context) => BrowseBloc(
+              searchRepository,
+              userBooksRepository,
+            ),
           ),
         ],
         child: App(appEnvironment: environment),
